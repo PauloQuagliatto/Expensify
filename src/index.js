185dd0297f9ff6327c98fb69678ffaee46d871fd
PauloQuagliatto <pquagliatto17@gmail.com>
2020-08-16 19:05:27 -0300

    ah shit, here we go again

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import 'normalize.css/normalize.css';
+import 'react-dates/lib/css/_datepicker.css';
+import './firebase/firebase';
 
-ReactDOM.render(
+const store = configureStore();
+
+const jsx = (
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  </React.StrictMode>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(jsx, document.getElementById('app'));
